8a3e06f9d08ee3a071ad81f1ad9e11c5a214b802
liuhuan <liuhuan@csii.com.cn>
2020-12-12 01:30:47 +0800

    -

--- src/right-side.vue
+++ src/right-side.vue
@@ -1,18 +1,25 @@
 <template>
   <div class="rightSideWrap">
-    <div class="rightSideView">
+    <div class="rightSideView" v-show="!descView.title">
+      <div class="routerViewWrap">
+        <div class="routerView">
+          <router-view></router-view>
+        </div>
+      </div>
+    </div>
+    <div class="rightSideView" v-show="descView.title">
       <div class="labelView">
         <span v-text="descView.title"></span>
       </div>
       <div class="routerViewWrap">
-        <span v-show="descView.title">Demo</span>
+        <span>Demo</span>
         <div class="routerView">
           <router-view></router-view>
         </div>
       </div>
       <div class="descView">
-         <div class="descView-code" v-text="descView.code"></div>
-         <div class="descView-params" v-html="descView.params"></div>
+        <div class="descViewCode" v-text="descView.code"></div>
+        <div class="descViewParams" v-html="descView.params"></div>
       </div>
     </div>
   </div>
@@ -38,7 +45,7 @@ export default {
   },
   created() {
     this.routeChange();
-  },
+    },
   methods: {
     routeChange: function() {
       switch (this.$route.path) {
@@ -46,7 +53,8 @@ export default {
           this.descView = {
             title: "日历组件",
             code: `<IworkCalendar :isInitNow="false" iworkData="date" :format="format" :level="level"></IworkCalendar>`,
-            params:`<pre>
+            params: `
+            <pre>
               isInitNow:是否使用初始化时间标志
               iworkData:初始化时间,该属性生效需要依赖 isInitNow
               format:格式化时间
@@ -54,19 +62,19 @@ export default {
               备注:isInitNow 为false  iworkData不起作用且输入框为空
                   isInitNow 为true   iworkData不为空 则使用iworkData 若iworkData为空,日期为当前日期
               模拟数据:
-                    {
-                      date: "2020/01/01",   //子组件会自动将日期传给父组件
-                      format: "yyyy/MM/dd",
-                      level: 3, //Y,M,D
-                    }
-            </pre>`
+              {
+                date: "2020/01/01",   //子组件会自动将日期传给父组件
+                format: "yyyy/MM/dd",
+                level: 3, //Y,M,D
+              }
+            </pre>`,
           };
           break;
         case "/iworkUpload":
           this.descView = {
             title: "上传组件",
             code: ` <IworkUpload file="file" :isMultiple="false" :isHand="true" @upload="upload"></IworkUpload>`,
-            params:`
+            params: `
             <pre>
               file:存储文件变量
               isMultiple:是否允许多文件上传
@@ -75,13 +83,12 @@ export default {
               备注:isInitNow 为false  iworkData不起作用且输入框为空
                   isInitNow 为true   iworkData不为空 则使用iworkData 若iworkData为空,日期为当前日期
               模拟数据:
-                    {
-                      file: [], ,   //选择的文件，子组件自动传递父组件，可能为数组，也可能为对象，取决于isMultiple
-                      isMultiple: false, //默认是单文件上传
-                      upload: function(file) { //file 上传的文件
-                      },
-                    }
-            </pre>`
+              {
+                file: [], ,   //选择的文件，子组件自动传递父组件，可能为数组，也可能为对象，取决于isMultiple
+                isMultiple: false, //默认是单文件上传
+                upload: function(file) { //file 上传的文件
+              }
+            </pre>`,
           };
           break;
         default:
@@ -142,13 +149,27 @@ $rightWidth: calc(100% - 2%);
         margin: auto;
       }
     }
-    .descView{
-        width: 90%;
-        margin: 5vh auto;
-        border: 1px #eeeefa solid;
-        height: 200px;
-        border-radius: 8px;
+    .descView {
+      width: 90%;
+      margin: 5vh auto;
+      border: 1px #eeeefa solid;
+      border-radius: 8px;
+      .descViewCode{
+        background: #eeeefa;
+        text-align: center;
+        line-height: 40px;
+        height: 40px;
+        color:blue;
+        font-weight: 600;
       }
+      .descViewParams{
+        text-align: left;
+        margin: 3vh 0;
+        pre{
+          text-align: left;
+        }
+      }
+    }
   }
 }
 </style>
