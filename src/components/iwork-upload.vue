0fe8dfc7b80c94ef9e738f077b2ffb434a62dcbc
liuhuan <liuhuan@csii.com.cn>
2021-08-25 11:04:43 +0800

    修复问题  上传

--- src/components/iwork-upload.vue
+++ src/components/iwork-upload.vue
@@ -1,254 +1,237 @@
 <template>
-  <div class="iwork-upload-wrapper">
-    <input
-      ref="iworkUploadInput"
-      class="iwork-upload-input"
-      type="text"
-      name="fileName"
-      v-model="fileName"
-      @keydown="forbiddenInput($event)"
-    />
-    <span
-      v-if="!isHand"
-      class="iwork-upload-span iwork-upload-not-hand-span"
-      @click="importFile"
-      @mousedown="preventFocus($event)"
-    >
-      <i class="iwork-upload-upload"></i>
-    </span>
-    <span
-      v-if="isHand"
-      class="iwork-upload-span iwork-upload-hand-span"
-      @mousedown="preventFocus($event)"
-    >
-      <i class="iwork-upload-upload" @click="importFile"></i>
-      <i class="iwork-upload-hand" @click="uploadNow"></i>
-    </span>
-    <input
-      type="file"
-      ref="fileInput"
-      @change="getFile"
-      v-show="fileInputShow"
-      :multiple="isMultiple"
-    />
-    <div
-      class="iwork-upload-multiple-wrapper"
-      @mousedown="preventFocus($event)"
-      v-show="isMultiple&&fileResource.length>1"
-    >
-      <ul class="iwork-upload-multiple-ul">
-        <li class="iwork-upload-multiple-li" v-for="(item,index) of fileResource" :key="index">
-          <span class="iwork-upload-multiple-left">{{item.name}}</span>
-          <span class="iwork-upload-multiple-right" @click="delFileResourceOne(index)">X</span>
-        </li>
-      </ul>
+    <div class="iwork-upload-wrapper">
+        <input ref="iworkUploadInput" class="iwork-upload-input" type="text" name="fileName" v-model="fileName"
+            @keydown="forbiddenInput($event)" @focus="isOpen=true" @blur="isOpen=false"/>
+        <span v-if="!isHand" class="iwork-upload-span iwork-upload-not-hand-span" @click="importFile"
+            @mousedown="preventFocus($event)">
+            <i class="iwork-upload-upload"></i>
+        </span>
+        <span v-if="isHand" class="iwork-upload-span iwork-upload-hand-span" @mousedown="preventFocus($event)">
+            <i class="iwork-upload-upload" @click="importFile"></i>
+            <i class="iwork-upload-hand" @click="uploadNow"></i>
+        </span>
+        <input type="file" ref="fileInput" @change="getFile" v-show="fileInputShow" :multiple="isMultiple" />
+        <div class="iwork-upload-multiple-wrapper" @mousedown="preventFocus($event)"
+            v-show="isOpen&&isMultiple&&fileResource.length>1">
+            <ul class="iwork-upload-multiple-ul">
+                <li class="iwork-upload-multiple-li" v-for="(item,index) of fileResource" :key="index">
+                    <span class="iwork-upload-multiple-left">{{item.name}}</span>
+                    <span class="iwork-upload-multiple-right" @click.stop="delFileResourceOne(index)">X</span>
+                </li>
+            </ul>
+        </div>
     </div>
-  </div>
 </template>
 <script>
 export default {
-  name: "IworkUpload",
-  props: {
-    isMultiple: {
-      type: Boolean,
-      default: true,
+    name: "IworkUpload",
+    props: {
+        isMultiple: {
+            type: Boolean,
+            default: true,
+        },
+        isHand: {
+            Boolean: Boolean,
+            default: true,
+        },
+        file: {},
     },
-    isHand:{
-      Boolean:Boolean,
-      default:true
+    data() {
+        return {
+            fileInputShow: false,
+            // 文件上传
+            fileResource: [],
+            // 多选文件列表显示标志
+            isOpen:false
+        };
     },
-    file: {}
-  },
-  data() {
-    return {
-      fileInputShow: false,
-      // 文件上传
-      fileResource: [],
-    };
-  },
-  watch: {
-    fileResource(n, o) {
-      if(this.isMultiple&&n instanceof FileList){
-        n = Array.prototype.slice.call(this.fileResource);
-      }
-      this.$parent[this.file] = n;
+    watch: {
+        fileResource(n, o) {
+            if (this.isMultiple && n instanceof FileList) {
+                n = Array.prototype.slice.call(this.fileResource);
+            }
+            this.$parent[this.file] = n;
+        },
     },
-  },
-  computed: {
-    fileName: function() {
-      if (this.isMultiple) {
-        let returnStr = [];
-        if (this.fileResource instanceof File) {
-          return this.fileResource.name;
-        } else {
-          this.fileResource.forEach((fileResource) => {
-            returnStr.push(fileResource.name);
-          });
-          return returnStr.join(",");
-        }
-      } else {
-        return this.fileResource.name;
-      }
+    computed: {
+        fileName: function () {
+            if (this.isMultiple) {
+                let returnStr = [];
+                if (this.fileResource instanceof File) {
+                    return this.fileResource.name;
+                } else {
+                    Array.from(this.fileResource).forEach((fileResource) => {
+                        returnStr.push(fileResource.name);
+                    });
+                    return returnStr.join(",");
+                }
+            } else {
+                return this.fileResource.name;
+            }
+        },
     },
-  },
-  methods: {
-    // 元素点击,防止失去焦点操作
-    // 阻止默认失去焦点事件
-    preventFocus(e) {
-      e.preventDefault();
+    methods: {
+        // 元素点击,防止失去焦点操作
+        // 阻止默认失去焦点事件
+        preventFocus(e) {
+            e.preventDefault();
+        },
+        // 文件禁止手动输入
+        forbiddenInput(e) {
+            e.preventDefault();
+        },
+        // 导入文件
+        importFile() {
+            let fileInput = this.$refs.fileInput;
+            this.$refs.iworkUploadInput.focus();
+            // 清除数据，让change事件触发
+            fileInput.value = "";
+            fileInput.click();
+        },
+        // 立即上传
+        uploadNow() {
+            this.$emit("upload", this.fileResource);
+        },
+        // 获取文件
+        getFile() {
+            let fileInput = this.$refs.fileInput;
+            if (this.isMultiple) {
+                if(!Array.isArray(this.fileResource)){
+                    this.fileResource = []
+                }
+                Array.from(fileInput.files).forEach(file=>{
+                    this.fileResource.push(file)
+                })
+            } else {
+                this.fileResource = {};
+                let file = fileInput.files[0];
+                this.fileResource = file;
+            }
+        },
+        // 删除多选的文件
+        delFileResourceOne(fileIndex) {
+            this.fileResource = Array.prototype.slice.call(this.fileResource);
+            this.fileResource.splice(fileIndex, 1);
+            // var  formData = new FormData();
+            // for(var i=0;i<this.fileResource.length;i++){
+            //   formData.append('file', this.fileResource[i], this.fileResource[i].name);
+            // }
+            // this.log(formData.get('file'))
+        },
     },
-    // 文件禁止手动输入
-    forbiddenInput(e) {
-      e.preventDefault();
-    },
-    // 导入文件
-    importFile() {
-      let fileInput = this.$refs.fileInput;
-      this.$refs.iworkUploadInput.focus();
-      // 清除数据，让change事件触发
-      fileInput.value = "";
-      fileInput.click();
-    },
-    // 立即上传
-    uploadNow(){
-      this.$emit("upload",this.fileResource);
-    },
-    // 获取文件
-    getFile() {
-      let fileInput = this.$refs.fileInput;
-      if (this.isMultiple) {
-        this.fileResource = [];
-        this.fileResource = fileInput.files;
-      } else {
-        this.fileResource = {};
-        let file = fileInput.files[0];
-        this.fileResource = file;
-      }
-    },
-    // 删除多选的文件
-    delFileResourceOne(fileIndex){
-      this.fileResource = Array.prototype.slice.call(this.fileResource);
-      this.fileResource.splice(fileIndex,1);
-      // var  formData = new FormData();
-      // for(var i=0;i<this.fileResource.length;i++){
-      //   formData.append('file', this.fileResource[i], this.fileResource[i].name);
-      // }
-      // this.log(formData.get('file'))
-    }
-  },
 };
 </script>
 <style lang="scss" scoped>
 .iwork-upload-wrapper {
-  position: relative;
-  font-size: 14px;
-  .iwork-upload-input {
     position: relative;
-    display: inline-block;
-    width: 100%;
-    height: 32px;
-    line-height: 1.5;
-    text-indent: 5px;
     font-size: 14px;
-    border: 1px solid #dcdee2;
-    border-radius: 4px;
-    color: #515a6e;
-    background-color: #fff;
-    background-image: none;
-    cursor: text;
-    transition: border 0.2s ease-in-out, background 0.2s ease-in-out,
-      box-shadow 0.2s ease-in-out;
-    outline: #57a3f3;
-  }
-  .iwork-upload-input:hover,
-  .iwork-upload-input:focus {
-    border-color: #57a3f3;
-  }
-  .iwork-upload-span{
-    position: absolute;
-    display: block;
-    right: 0;
-    top: 0;
-    height: 100%;
-  }
-  .iwork-upload-not-hand-span {
-    width: 30px;
-    cursor: pointer;
-    .iwork-upload-upload {
-      position: absolute;
-      top: 0;
-      left: 0;
-      right: 0;
-      bottom: 0;
-      background: url(../assets/upload.png) no-repeat;
-      background-position: center;
-    }
-  }
-  .iwork-upload-hand-span {
-    width: 60px;
-    cursor: pointer;
-    i{
-      display: inline-block;
-      width: 50%;
-      height: 100%;
-    }
-    .iwork-upload-upload {
-      background: url(../assets/upload.png) no-repeat;
-      background-position: center;
-    }
-    .iwork-upload-hand {
-      background: url(../assets/hand.png) no-repeat;
-      background-position: center;
-    }
-  }
-  .iwork-upload-multiple-wrapper {
-    position: absolute;
-    margin-top: 5px;
-    width: 216px;
-    height: 130px;
-    border: 1px #cccccc solid;
-    border-radius: 3px;
-    box-shadow: #bbbbbb 0px 0px 20px 2px;
-    z-index: 999;
-    background: white;
-    overflow: scroll;
-    text-align: left;
-    .iwork-upload-multiple-ul {
-      width: 100%;
-      position: absolute;
-      margin: 0;
-      padding: 0px 0px;
-      list-style: none;
-      .iwork-upload-multiple-li {
-        width: 100%;
+    .iwork-upload-input {
         position: relative;
-        height: 25px;
-        line-height: 25px;
-        color: #666666;
-        .iwork-upload-multiple-left {
-          position: absolute;
-          left: 8px;
-          display: inline-block;
-          width: 80%;
-          height: 25px;
-          overflow: hidden;
-          white-space: nowrap;
-          text-overflow: ellipsis;
-          cursor: default;
-        }
-        .iwork-upload-multiple-right {
-          position: absolute;
-          right: 8px;
-          cursor: pointer;
-        }
-      }
-      .iwork-upload-multiple-li:hover{
-        background-color: #D4E7FA;
-      }
-      .iwork-upload-multiple-li:not(:last-child){
-        border-bottom: 1px #dcdee2 solid;
-      }
+        display: inline-block;
+        width: 100%;
+        height: 32px;
+        line-height: 1.5;
+        text-indent: 5px;
+        font-size: 14px;
+        border: 1px solid #dcdee2;
+        border-radius: 4px;
+        color: #515a6e;
+        background-color: #fff;
+        background-image: none;
+        cursor: text;
+        transition: border 0.2s ease-in-out, background 0.2s ease-in-out,
+            box-shadow 0.2s ease-in-out;
+        outline: #57a3f3;
     }
-  }
+    .iwork-upload-input:hover,
+    .iwork-upload-input:focus {
+        border-color: #57a3f3;
+    }
+    .iwork-upload-span {
+        position: absolute;
+        display: block;
+        right: 0;
+        top: 0;
+        height: 100%;
+    }
+    .iwork-upload-not-hand-span {
+        width: 30px;
+        cursor: pointer;
+        .iwork-upload-upload {
+            position: absolute;
+            top: 0;
+            left: 0;
+            right: 0;
+            bottom: 0;
+            background: url(../assets/upload.png) no-repeat;
+            background-position: center;
+        }
+    }
+    .iwork-upload-hand-span {
+        width: 60px;
+        cursor: pointer;
+        i {
+            display: inline-block;
+            width: 50%;
+            height: 100%;
+        }
+        .iwork-upload-upload {
+            background: url(../assets/upload.png) no-repeat;
+            background-position: center;
+        }
+        .iwork-upload-hand {
+            background: url(../assets/hand.png) no-repeat;
+            background-position: center;
+        }
+    }
+    .iwork-upload-multiple-wrapper {
+        position: absolute;
+        margin-top: 5px;
+        width: 100%;
+        height: 130px;
+        border: 1px #cccccc solid;
+        border-radius: 3px;
+        box-shadow: #bbbbbb 0px 0px 20px 2px;
+        z-index: 999;
+        background: white;
+        overflow: auto;
+        text-align: left;
+        .iwork-upload-multiple-ul {
+            width: 100%;
+            position: absolute;
+            margin: 0;
+            padding: 0px 0px;
+            list-style: none;
+            .iwork-upload-multiple-li {
+                width: 100%;
+                position: relative;
+                height: 25px;
+                line-height: 25px;
+                color: #666666;
+                .iwork-upload-multiple-left {
+                    position: absolute;
+                    left: 8px;
+                    display: inline-block;
+                    width: 80%;
+                    height: 25px;
+                    overflow: hidden;
+                    white-space: nowrap;
+                    text-overflow: ellipsis;
+                    cursor: default;
+                }
+                .iwork-upload-multiple-right {
+                    position: absolute;
+                    right: 8px;
+                    cursor: pointer;
+                }
+            }
+            .iwork-upload-multiple-li:hover {
+                background-color: #d4e7fa;
+            }
+            .iwork-upload-multiple-li:not(:last-child) {
+                border-bottom: 1px #dcdee2 solid;
+            }
+        }
+    }
 }
 </style>
--- src/globalMedthods/globalMedthods.js
+++ src/globalMedthods/globalMedthods.js
@@ -1,112 +1,112 @@
 // 改变主题颜色
 const changeColorTheme = function (id, href) {
-  var dom = document.getElementById(id);
-  dom.href = href
+    var dom = document.getElementById(id);
+    dom.href = href
 }
 // 判断指定节点是否在父节点中,以及他是否就是含有
 const isHasInParent = function (parentId, dom) {
-  // 传入标签是否是DOM对象
-  if (!(dom instanceof HTMLElement)) {
-    return false;
-  }
-  // 自身是否就是含有
-  if(parentId == dom.id){
-    return true;
-  }
-  // 父级标签是否是body,是着停止返回集合,反之继续
-  if ('BODY' !== dom.parentElement.nodeName) {
-    if(parentId == dom.parentElement.id){
-      return true;
+    // 传入标签是否是DOM对象
+    if (!(dom instanceof HTMLElement)) {
+        return false;
     }
-    // 再上一层寻找
-    return isHasInParent(parentId, dom.parentElement)
-  }else{
-    return false;
-  }
+    // 自身是否就是含有
+    if (parentId == dom.id) {
+        return true;
+    }
+    // 父级标签是否是body,是着停止返回集合,反之继续
+    if ('BODY' !== dom.parentElement.nodeName) {
+        if (parentId == dom.parentElement.id) {
+            return true;
+        }
+        // 再上一层寻找
+        return isHasInParent(parentId, dom.parentElement)
+    } else {
+        return false;
+    }
 }
 // 获取所有父节点
-const getAllParents = function (dom,parentTagList = []) {
-  // 传入标签是否是DOM对象
-  if (!(dom instanceof HTMLElement)) {
-    return console.error('receive only HTMLElement');
-  }
-  // 父级标签是否是body,是着停止返回集合,反之继续
-  if ('BODY' !== dom.parentElement.nodeName) {
-    // 放入集合
-    parentTagList.push(dom.parentElement)
-    // 再上一层寻找
-    return getParentTag(dom.parentElement, parentTagList)
-  }else{
-    return parentTagList;
-  }
+const getAllParents = function (dom, parentTagList = []) {
+    // 传入标签是否是DOM对象
+    if (!(dom instanceof HTMLElement)) {
+        return console.error('receive only HTMLElement');
+    }
+    // 父级标签是否是body,是着停止返回集合,反之继续
+    if ('BODY' !== dom.parentElement.nodeName) {
+        // 放入集合
+        parentTagList.push(dom.parentElement)
+        // 再上一层寻找
+        return getParentTag(dom.parentElement, parentTagList)
+    } else {
+        return parentTagList;
+    }
 }
 
 /**
  * 功能: 获取元素属性值
  * 参数: prop 属性
-*/
-HTMLElement.prototype.getStyle = function(prop){
-    if(!prop){
-        return window.getComputedStyle(this,null) || this.currentStyle
+ */
+HTMLElement.prototype.getStyle = function (prop) {
+    if (!prop) {
+        return window.getComputedStyle(this, null) || this.currentStyle
     }
-    if(window.getComputedStyle){
-        return window.getComputedStyle(this,null)[prop]
-    }else{
+    if (window.getComputedStyle) {
+        return window.getComputedStyle(this, null)[prop]
+    } else {
         return this.currentStyle[prop];
     }
 }
 
 /**
  * 功能: RGB转换为16进制
-*/
+ */
 String.prototype.colorHex = function () {
     // RGB颜色值的正则
     var reg = /^(rgb|RGB)/;
     var color = this;
     if (reg.test(color)) {
-      var strHex = "#";
-      // 把RGB的3个数值变成数组
-      var colorArr = color.replace(/(?:\(|\)|rgb|RGB)*/g, "").split(",");
-      // 转成16进制
-      for (var i = 0; i < colorArr.length; i++) {
-        var hex = Number(colorArr[i]).toString(16);
-        if (hex === "0") {
-          hex += hex;
+        var strHex = "#";
+        // 把RGB的3个数值变成数组
+        var colorArr = color.replace(/(?:\(|\)|rgb|RGB)*/g, "").split(",");
+        // 转成16进制
+        for (var i = 0; i < colorArr.length; i++) {
+            var hex = Number(colorArr[i]).toString(16);
+            if (hex === "0") {
+                hex += hex;
+            }
+            strHex += hex;
         }
-        strHex += hex;
-      }
-      return strHex;
+        return strHex;
     } else {
-      return String(color);
+        return String(color);
     }
 };
 
 /**
  * 功能: 16进制转换为RGB
-*/
+ */
 String.prototype.colorRgb = function () {
-  // 16进制颜色值的正则
-  var reg = /^#([0-9a-fA-f]{3}|[0-9a-fA-f]{6})$/;
-  // 把颜色值变成小写
-  var color = this.toLowerCase();
-  if (reg.test(color)) {
-    // 如果只有三位的值，需变成六位，如：#fff => #ffffff
-    if (color.length === 4) {
-      var colorNew = "#";
-      for (var i = 1; i < 4; i += 1) {
-        colorNew += color.slice(i, i + 1).concat(color.slice(i, i + 1));
-      }
-      color = colorNew;
+    // 16进制颜色值的正则
+    var reg = /^#([0-9a-fA-f]{3}|[0-9a-fA-f]{6})$/;
+    // 把颜色值变成小写
+    var color = this.toLowerCase();
+    if (reg.test(color)) {
+        // 如果只有三位的值，需变成六位，如：#fff => #ffffff
+        if (color.length === 4) {
+            var colorNew = "#";
+            for (var i = 1; i < 4; i += 1) {
+                colorNew += color.slice(i, i + 1).concat(color.slice(i, i + 1));
+            }
+            color = colorNew;
+        }
+        // 处理六位的颜色值，转为RGB
+        var colorChange = [];
+        for (var i = 1; i < 7; i += 2) {
+            colorChange.push(parseInt("0x" + color.slice(i, i + 2)));
+        }
+        return "RGB(" + colorChange.join(",") + ")";
+    } else {
+        return color;
     }
-    // 处理六位的颜色值，转为RGB
-    var colorChange = [];
-    for (var i = 1; i < 7; i += 2) {
-      colorChange.push(parseInt("0x" + color.slice(i, i + 2)));
-    }
-    return "RGB(" + colorChange.join(",") + ")";
-  } else {
-    return color;
-  }
 };
 // 全局字符串增加替换方法
 String.prototype.replaceAll = function (s1, s2) {
@@ -119,21 +119,21 @@ String.prototype.replaceAll = function (s1, s2) {
  * */
 // 日期格式化
 Date.prototype.format = function (format) {
-　　var args = {
-　　　　"M+": this.getMonth() + 1,
-　　　　"d+": this.getDate(),
-　　　　"h+": this.getHours(),
-　　　　"m+": this.getMinutes(),
-　　　　"s+": this.getSeconds(),
-　　　　"q+": Math.floor((this.getMonth() + 3) / 3), //quarter
-　　　　"S": this.getMilliseconds()
-　　};
-　　if (/(y+)/.test(format)) format = format.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
-　　for (var i in args) {
-　　　　var n = args[i];
-　　　　if (new RegExp("(" + i + ")").test(format)) format = format.replace(RegExp.$1, RegExp.$1.length == 1 ? n : ("00" + n).substr(("" + n).length));
-　　}
-　　return format;
+    var args = {
+        "M+": this.getMonth() + 1,
+        "d+": this.getDate(),
+        "h+": this.getHours(),
+        "m+": this.getMinutes(),
+        "s+": this.getSeconds(),
+        "q+": Math.floor((this.getMonth() + 3) / 3), //quarter
+        "S": this.getMilliseconds()
+    };
+    if (/(y+)/.test(format)) format = format.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+    for (var i in args) {
+        var n = args[i];
+        if (new RegExp("(" + i + ")").test(format)) format = format.replace(RegExp.$1, RegExp.$1.length == 1 ? n : ("00" + n).substr(("" + n).length));
+    }
+    return format;
 };
 
 //生成指定长度由随机数字组成的字符串的方法
@@ -147,14 +147,14 @@ export function getRandomNumber(n) {
 };
 
 // 生成随机ID
-const getRandomUniqueID = function(){
-    return new Date().getTime()+getRandomNumber(5)
+const getRandomUniqueID = function () {
+    return new Date().getTime() + getRandomNumber(5)
 }
 
 export default {
-  changeColorTheme,
-  getAllParents,
-  isHasInParent,
-  getRandomNumber,
-  getRandomUniqueID
+    changeColorTheme,
+    getAllParents,
+    isHasInParent,
+    getRandomNumber,
+    getRandomUniqueID
 }
